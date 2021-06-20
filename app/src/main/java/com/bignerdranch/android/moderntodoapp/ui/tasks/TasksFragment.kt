29839e7dbf4f5198ce13fb594b9a701839d545c3
Djumabaevs <djumabaevb@gmail.com>
2021-06-20 12:42:29 +0600

    bind(view) in fragment

--- app/src/main/java/com/bignerdranch/android/moderntodoapp/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/bignerdranch/android/moderntodoapp/ui/tasks/TasksFragment.kt
@@ -1,12 +1,32 @@
 package com.bignerdranch.android.moderntodoapp.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bignerdranch.android.moderntodoapp.R
+import com.bignerdranch.android.moderntodoapp.databinding.FragmentTasksBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
     private val viewModel: TasksViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTasksBinding.bind(view)
+
+        val taskAdapter = TasksAdapter()
+
+        binding.apply {
+            recyclerViewTasks.apply {
+                adapter = taskAdapter
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+    }
 }
