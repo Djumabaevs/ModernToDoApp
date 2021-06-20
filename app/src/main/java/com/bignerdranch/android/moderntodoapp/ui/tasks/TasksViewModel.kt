5d3e3e29c97ef40141ad676574ca3951f23e9395
Djumabaevs <djumabaevb@gmail.com>
2021-06-20 12:49:25 +0600

    flow asLiveData

--- app/src/main/java/com/bignerdranch/android/moderntodoapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/moderntodoapp/ui/tasks/TasksViewModel.kt
@@ -2,6 +2,7 @@ package com.bignerdranch.android.moderntodoapp.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.bignerdranch.android.moderntodoapp.data.TaskDao
 
 class TasksViewModel @ViewModelInject constructor(
@@ -9,5 +10,6 @@ class TasksViewModel @ViewModelInject constructor(
 )
     : ViewModel() {
 
+        val tasks = taskDao.getTasks().asLiveData()
 
 }
