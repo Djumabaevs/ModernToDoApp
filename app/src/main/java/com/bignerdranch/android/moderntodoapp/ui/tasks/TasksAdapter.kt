601881bfe27e35f9d4d4d16ad1dbd7262767ee59
Djumabaevs <djumabaevb@gmail.com>
2021-06-20 12:05:12 +0600

    binding.apply{}

--- app/src/main/java/com/bignerdranch/android/moderntodoapp/ui/tasks/TasksAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/moderntodoapp/ui/tasks/TasksAdapter.kt
@@ -1,14 +1,21 @@
 package com.bignerdranch.android.moderntodoapp.ui.tasks
 
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.moderntodoapp.data.Task
 import com.bignerdranch.android.moderntodoapp.databinding.ItemTaskBinding
 
-class TasksAdapter : ListAdapter<Task,> {
+class TasksAdapter : ListAdapter<Task, TasksAdapter.TasksViewHolder> {
 
     class TasksViewHolder(private val binding: ItemTaskBinding) : RecyclerView.ViewHolder(binding.root) {
         fun bind(task: Task) {
+            binding.apply {
+                checkBoxCompleted.isChecked = task.completed
+                textViewName.text = task.name
+                textViewName.paint.isStrikeThruText = task.completed
+                labelPriority.isVisible = task.important
+            }
 
         }
     }
