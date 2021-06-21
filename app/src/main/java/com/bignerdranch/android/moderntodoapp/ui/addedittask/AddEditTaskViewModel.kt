c4a6cb0333bbbefdb75c35770e30312d8d26207a
Djumabaevs <djumabaevb@gmail.com>
2021-06-22 00:16:49 +0600

    task?.name ?: ""

--- app/src/main/java/com/bignerdranch/android/moderntodoapp/ui/addedittask/AddEditTaskViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/moderntodoapp/ui/addedittask/AddEditTaskViewModel.kt
@@ -1,17 +1,20 @@
 package com.bignerdranch.android.moderntodoapp.ui.addedittask
 
 import androidx.hilt.Assisted
+import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.bignerdranch.android.moderntodoapp.data.Task
 import com.bignerdranch.android.moderntodoapp.data.TaskDao
 
-class AddEditTaskViewModel(
+class AddEditTaskViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao,
    @Assisted private val state: SavedStateHandle
 ) : ViewModel() {
 
     val task = state.get<Task>("task")
+
+    var taskName = state.get<String>("taskName") ?: task?.name ?: ""
 
 
 }
