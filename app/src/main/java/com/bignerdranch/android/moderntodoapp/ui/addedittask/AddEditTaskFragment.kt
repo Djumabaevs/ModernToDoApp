59b05c44eed96dcb17e9f8d9ba97ea2d727ad923
Djumabaevs <djumabaevb@gmail.com>
2021-06-22 00:35:20 +0600

    jump drawables to current state

--- app/src/main/java/com/bignerdranch/android/moderntodoapp/ui/addedittask/AddEditTaskFragment.kt
+++ app/src/main/java/com/bignerdranch/android/moderntodoapp/ui/addedittask/AddEditTaskFragment.kt
@@ -1,7 +1,27 @@
 package com.bignerdranch.android.moderntodoapp.ui.addedittask
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.bignerdranch.android.moderntodoapp.R
+import com.bignerdranch.android.moderntodoapp.databinding.FragmentAddEditTaskBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class AddEditTaskFragment : Fragment(R.layout.fragment_add_edit_task) {
+
+    private val viewModel: AddEditTaskViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentAddEditTaskBinding.bind(view)
+
+        binding.apply {
+            editTextTaskName.setText(viewModel.taskName)
+            checkBoxImportant.isChecked = viewModel.taskImportance
+            checkBoxImportant.jumpDrawablesToCurrentState()
+        }
+    }
 }
