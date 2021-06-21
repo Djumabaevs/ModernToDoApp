e23ae6e56e4206afc3a3421124e376dc9cc34efe
Djumabaevs <djumabaevb@gmail.com>
2021-06-22 00:09:55 +0600

    savedStateHandle

--- app/src/main/java/com/bignerdranch/android/moderntodoapp/ui/addedittask/AddEditTaskViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/moderntodoapp/ui/addedittask/AddEditTaskViewModel.kt
@@ -1,4 +1,17 @@
 package com.bignerdranch.android.moderntodoapp.ui.addedittask
 
-class AddEditTaskViewModel  {
+import androidx.hilt.Assisted
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.bignerdranch.android.moderntodoapp.data.Task
+import com.bignerdranch.android.moderntodoapp.data.TaskDao
+
+class AddEditTaskViewModel(
+    private val taskDao: TaskDao,
+   @Assisted private val state: SavedStateHandle
+) : ViewModel() {
+
+    val task = state.get<Task>("task")
+
+
 }
