2344e7e9bca5889ba81b267677a0eb7d69b3724e
Djumabaevs <djumabaevb@gmail.com>
2021-06-22 10:18:35 +0600

    findNavController().navigate(action) to perform safety compiler

--- app/src/main/java/com/bignerdranch/android/moderntodoapp/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/bignerdranch/android/moderntodoapp/ui/tasks/TasksFragment.kt
@@ -9,6 +9,7 @@ import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,6 +17,7 @@ import com.bignerdranch.android.moderntodoapp.R
 import com.bignerdranch.android.moderntodoapp.data.SortOrder
 import com.bignerdranch.android.moderntodoapp.data.Task
 import com.bignerdranch.android.moderntodoapp.databinding.FragmentTasksBinding
+import com.bignerdranch.android.moderntodoapp.util.exhaustive
 import com.bignerdranch.android.moderntodoapp.util.onQueryTextChanged
 import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
@@ -77,7 +79,15 @@ class TasksFragment : Fragment(R.layout.fragment_tasks), TasksAdapter.OnItemClic
                                 viewModel.onUndoDeleteClick(event.task)
                             }.show()
                     }
-                }
+                    is TasksViewModel.TasksEvent.NavigateToAddTaskScreen -> {
+                        val action = TasksFragmentDirections.actionTasksFragmentToAddEditTaskFragment()
+                        findNavController().navigate(action)
+                    }
+                    is TasksViewModel.TasksEvent.NavigateToEditTaskScreen -> {
+                        val action = TasksFragmentDirections.actionTasksFragmentToAddEditTaskFragment(event.task)
+                        findNavController().navigate(action)
+                    }
+                }.exhaustive
             }
         }
 
