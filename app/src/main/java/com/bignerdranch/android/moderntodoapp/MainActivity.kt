d69d1ac0b456305da25dace81f911e5c99588e2d
Djumabaevs <djumabaevb@gmail.com>
2021-06-22 10:27:27 +0600

    navigate up in onSupportNavigateUpt overriden method

--- app/src/main/java/com/bignerdranch/android/moderntodoapp/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/moderntodoapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,6 +21,13 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
 
         navController = navHostFragment.findNavController()
+
+        setupActionBarWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
 
     }
 }
